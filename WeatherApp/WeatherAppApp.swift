c7031a26da84041dc7fa75ca25e5b4278be8671f
josman185 <josman185@hotmail.com>
2024-09-25 17:58:39 -0600

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Jose Vargas on 9/25/24.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
